a1be75da897b14f8a6b8e8962f31adff57f62deb
Zhengyu Duan <670450301@qq.com>
2019-11-19 18:28:37 -0800

    test.php

--- sensors/test.php
+++ sensors/test.php
@@ -27,13 +27,14 @@ $sensors = new sensors($db);
 
 if($sensors->start()){
     // set response code - 200 OK
-    http_response_code(200);
- 	echo "1";
-  //   echo json_encode(array("message" => "Station now Running."));
+    // http_response_code(200);
+ 	;
+    echo json_encode(array("message" => "Station now Running."));
+    phpinfo();
 }else{
  
     // set response code - 404 Not found
-    http_response_code(404);
+    // http_response_code(404);
  	echo "2";
  
   //   // tell the user no products found
