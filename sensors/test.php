02bc263038740852822339fff99c7b855887818f
Zhengyu Duan <670450301@qq.com>
2019-11-19 18:17:00 -0800

    test.php

--- sensors/test.php
+++ sensors/test.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>test?</title>
-</head>
-<body>
 
 <?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ 
+// database connection will be here
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/sensors.php';
+ 
+// instantiate database and sensors object
+$database = new Database();
+$db = $database->getConnection();
+ 
+// initialize object
+$sensors = new sensors($db);
+ 
+
 if (extension_loaded('pdo')) 
 {
 	echo "success loaded pdo\n";
@@ -13,17 +24,27 @@ if (extension_loaded('pdo'))
     dl('pdo.so');
 }
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-include_once '../config/database.php';
-include_once '../objects/sensors.php';
- 
-$database = new Database();
-$db = $database->getConnection();
- 
-$sensors = new sensors($db);
- 
 
+// read products will be here
+// query products
+if($sensors->start()){
+    // set response code - 200 OK
+    http_response_code(200);
+ 
+    // show sensors data in json format
+    // echo json_encode(array("message" => "Data Generated."));
+    echo json_encode(array("message" => "Station now Running."));
+}
+ 
+// no products found will be here
+else{
+ 
+    // set response code - 404 Not found
+    http_response_code(404);
+ 
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "Failed.")
+    );
+}
 ?>
-</body>
-</html>
