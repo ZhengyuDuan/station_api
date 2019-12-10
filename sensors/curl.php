651ef6eabb3b07e27c12136f6d96fa3a2b606172
Zhengyu Duan <670450301@qq.com>
2019-12-10 12:24:14 -0800

    removed curl.php function

--- sensors/curl.php
+++ sensors/curl.php
@@ -3,20 +3,20 @@
         // this function call and sending data to another DB
         //TODO: following codes are copy from web, needs modify.
         //**************************************************************************
-        
+/*
 $data = array(
-    "status"=>1);                                                               
-$data_string = json_encode($data);                                                                                   
-                                                                                                                     
-$ch = curl_init('http://xckang.com/api/public/sensor/25');                                                                      
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',                                                                                
-    'Content-Length: ' . strlen($data_string))                                                                       
-);                                                                                                                   
-                                                                                                                     
+    "status"=>1);
+$data_string = json_encode($data);
+
+$ch = curl_init('http://xckang.com/api/public/sensor/25');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))
+);
+
 $result = curl_exec($ch);
 echo $result;
 ?>
@@ -30,3 +30,5 @@ echo $result;
         $url = "machine/".$id;
         $data = array("status"=>$newStatus);
         $this->postcurl($url,json_encode($data));
+*/
+echo "This api currently closed for public."
